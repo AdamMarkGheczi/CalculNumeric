e71a179c367331cf2d6ea9fa88662160476fa5ab
BlueNinja <blueninja4036@gmail.com>
2024-11-11 22:48:05 +0200

    Lab 05.11.2024

---
+++ 05.11.2024/Aproximari succesive/Aproximari succesive/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aproximari_succesive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] x = new double[50];
+            double eps;
+            int n = 0;
+
+            Console.WriteLine("Introduceti x_0");
+            x[0] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti epsilon");
+            eps = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"x[{n}] = {x[n]}");
+
+            do
+            {
+                n++;
+                x[n] = fi(x[n - 1]);
+                Console.WriteLine($"x[{n}] = {x[n]}");
+            } while (Math.Abs(x[n] - x[n - 1]) >= eps);
+
+            Console.ReadLine();
+
+        }
+
+        public static double fi(double x)
+        {
+            return Math.Sin(x) + 0.25;
+            //return (Math.Pow(x, 5) + 1) / 5;
+            //return NthRoot(x + 1, 3);
+            //return NthRoot(x + 1, 4);
+        }
+
+        public static double NthRoot(double x, double n)
+        {
+            return Math.Pow(x, 1 / n);
+        }
+    }
+}
---
+++ 05.11.2024/Heron/Heron/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heron
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, eps;
+            int n = 0;
+
+            double[] x = new double[50];
+
+            Console.WriteLine("Introduceti a: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti x[0]: ");
+            x[0] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti epsilon: ");
+            eps = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"x[{n}] = {x[n]}");
+            do
+            {
+                n++;
+                x[n] = (x[n - 1] + a / x[n - 1]) / 2;
+                Console.WriteLine($"x[{n}] = {x[n]}");
+
+            } while (Math.Abs(x[n] - x[n - 1]) >= eps);
+
+            Console.ReadLine();
+        }
+    }
+}
